9c2f4a9412d8e14b5d7abcf5446c034c2ea0eae7
spasenovskaa <ana.spasenovska@gmail.com>
2021-04-23 22:26:03 +0200

    Pets

---
+++ src/data/cats.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    name: 'Fish',
+    breed: 'tuxedo',
+    species: 'cat',
+    gender: 'male',
+    age: 20,
+    color: 'black/white',
+    weight: 13,
+    location: 'fourside',
+    notes: 'Sweer kitty. He loves getting his belly rubbed.'
+  },
+  {
+    name: 'Henry',
+    breed: 'tabby',
+    species: 'cat',
+    gender: 'male',
+    age: 20,
+    color: 'orange/white',
+    weight: 17,
+    location: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'Roger',
+    breed: 'tabby',
+    species: 'cat',
+    gender: 'male',
+    age: 20,
+    color: 'grey',
+    weight: 15,
+    location: 'threed',
+    notes: 'Super friendly'
+  },
+  {
+    name: 'KitKat',
+    breed: 'bombay',
+    species: 'cat',
+    gender: 'female',
+    age: 9,
+    color: 'black',
+    weight: 9,
+    location: 'threed',
+    notes: 'Super friendly'
+  }
+]
